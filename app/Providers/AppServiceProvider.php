90b0b2e1b93b08f929f99ac58bbb634f6f8404d5
giant-paw <giantpawbusiness@gmail.com>
2025-06-04 18:09:25 +0700

    api : auth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,29 +2,34 @@
 
 namespace App\Providers;
 
+use Dedoc\Scramble\Scramble;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Str;
+use Laravel\Sanctum\PersonalAccessToken;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+    
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
+   
     public function boot(): void
     {
         Paginator::useTailwind();
 
-        Gate::define('admin', function($user) {
+        Gate::define('admin', function ($user) {
             return $user->is_admin == true;
+        });
+
+        Scramble::configure()->routes(function (Route $route) {
+            return Str::startsWith($route->uri, 'api/');
         });
     }
 }
